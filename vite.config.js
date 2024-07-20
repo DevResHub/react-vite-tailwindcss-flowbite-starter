aadfc01e118b9282ad2aec6e6f80f0dc50f627f9
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-07-20 15:52:31 +0500

    Full Initialize

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const myPlugin = () => {
+  return {
+    name: 'my-plugin',
+    configureServer(server) {
+      server.httpServer.once('listening', () => {
+        console.log('Happy Hacking, 🫠 🫠 😴');
+      });
+    },
+  };
+};
+
+export default defineConfig({
+  plugins: [react(), myPlugin()],
+  server: {
+    port: 16000,
+    open: true,
+  },
+});
