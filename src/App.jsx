eea4011ac0d40b29573c21a95aa0a3c122dfb12f
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-07-22 22:03:43 +0500

    Flowbite Bug-Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
+import Nav from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Nav />
+      <Header />
       <Footer />
     </div>
   )
